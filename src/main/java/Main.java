ba46e417550604818f3a04bbea9e86347905bda3
Pavel Shilin <shilin_p.95@mail.ru>
2020-07-19 15:24:06 +0300

    Third step

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
+
     public static void main (String[] args){
         System.out.println("hello world1");
-
+        Worker worker1 = new Worker();
+        worker1.setFirstname("Ivan");
+        System.out.println(worker1.getFirstname());
     }
 }
---
+++ src/main/java/Worker.java
@@ -0,0 +1,25 @@
+public class Worker {
+    String firstname;
+    String secondname;
+    Departament departament;
+
+    public Worker(){
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getSecondname() {
+        return secondname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setSecondname(String secondname) {
+        this.secondname = secondname;
+    }
+}
